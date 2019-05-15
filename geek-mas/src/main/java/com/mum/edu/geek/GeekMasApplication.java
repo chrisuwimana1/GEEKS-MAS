e3eb8c03ea8fc31d2c010212ae2632d65c7b0911
Celestin Mbuyamba <celembuyamba@gmail.com>
2019-05-15 14:57:15 -0500

    Project initialization

---
+++ geek-mas/src/main/java/com/mum/edu/geek/GeekMasApplication.java
@@ -0,0 +1,13 @@
+package com.mum.edu.geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekMasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekMasApplication.class, args);
+    }
+
+}
---
+++ geek-mas/src/main/java/com/mum/edu/geek/domain/Course.java
@@ -0,0 +1,83 @@
+package com.mum.edu.geek.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Course {
+    private String id;
+    private String name;
+    private String description;
+    private int numberOfWeek;
+    private int numberOfSession;
+
+    @Id
+    @Column(name = "ID", nullable = false, length = 5)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "NAME", nullable = false, length = 80)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "DESCRIPTION", nullable = true, length = 200)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Basic
+    @Column(name = "NUMBER_OF_WEEK", nullable = false)
+    public int getNumberOfWeek() {
+        return numberOfWeek;
+    }
+
+    public void setNumberOfWeek(int numberOfWeek) {
+        this.numberOfWeek = numberOfWeek;
+    }
+
+    @Basic
+    @Column(name = "NUMBER_OF_SESSION", nullable = false)
+    public int getNumberOfSession() {
+        return numberOfSession;
+    }
+
+    public void setNumberOfSession(int numberOfSession) {
+        this.numberOfSession = numberOfSession;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return numberOfWeek == course.numberOfWeek &&
+                numberOfSession == course.numberOfSession &&
+                Objects.equals(id, course.id) &&
+                Objects.equals(name, course.name) &&
+                Objects.equals(description, course.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, numberOfWeek, numberOfSession);
+    }
+}
