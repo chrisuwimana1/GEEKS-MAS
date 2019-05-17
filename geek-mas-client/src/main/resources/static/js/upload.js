fff741a73b860eb881368c17d60ae42b1264b931
Halison da Silva Brito <halisonbrito@gmail.com>
2019-05-17 13:40:07 -0400

    Upload View

--- geek-mas-client/src/main/resources/static/js/upload.js
+++ geek-mas-client/src/main/resources/static/js/upload.js
@@ -1,10 +1,10 @@
-function upload(){
+function upload(url){
     var fileContent = new FormData();
     fileContent.append("file",$('input[type=file]')[0].files[0]);
     $.ajax({
         type: "POST",
         enctype:"multipart/form-data",
-        url: "http://localhost:8888/uploadAutoFile",
+        url: "http://localhost:8888/"+url,
         data: fileContent,
         processData: false,
         contentType: false,
