e92d391e1ce44de0938f94d29921351cd560abcf
Halison da Silva Brito <halisonbrito@gmail.com>
2019-05-19 19:53:35 -0400

    New implementation of file reading
    Jwt correction

--- geek-mas/src/main/java/com/mum/edu/geek/util/FileUtil.java
+++ geek-mas/src/main/java/com/mum/edu/geek/util/FileUtil.java
@@ -1,12 +1,18 @@
 package com.mum.edu.geek.util;
 
 import org.springframework.web.multipart.MultipartFile;
-
-import java.net.URI;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileUtil {
 
@@ -15,6 +21,32 @@ public class FileUtil {
         file.transferTo(path);
         List<String> list = Files.readAllLines(path);
         Files.delete(path);
-        return list;
+        return null;
     }
+
+    public static List<String> readFileFaster(MultipartFile file) throws Exception{
+
+        List<String> lines = new ArrayList<>();
+        CharBuffer charBuffer = null;
+
+        Path pathToRead = Paths.get("file.txt");
+        file.transferTo(pathToRead);
+
+        try (FileChannel fileChannel =(FileChannel) Files.newByteChannel(
+                pathToRead, EnumSet.of(StandardOpenOption.DELETE_ON_CLOSE))) {
+            MappedByteBuffer mappedByteBuffer = fileChannel
+                    .map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+
+            charBuffer = Charset.forName("UTF-8").decode(mappedByteBuffer);
+            Scanner scanner = new Scanner(charBuffer);
+            while(scanner.hasNextLine()){
+                lines.add(scanner.nextLine().toString());
+            }
+
+            mappedByteBuffer.clear();
+        }
+
+        return lines;
+    }
+
 }
