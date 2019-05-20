34fee944629077dfb9b173b5d68cc9d3ae389555
Celestin Mbuyamba <celembuyamba@gmail.com>
2019-05-20 16:53:16 -0500

    Change about StudentSectionId

--- geek-mas/src/main/java/com/mum/edu/geek/repository/StudentSectionRepository.java
+++ geek-mas/src/main/java/com/mum/edu/geek/repository/StudentSectionRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 @Repository
 public interface StudentSectionRepository extends JpaRepository<StudentSection, Integer> {
-    @Query(value = "select ss from StudentSection ss where ss.studentFacultyId.studentId = :studentId")
+    @Query(value = "select ss from StudentSection ss where ss.studentSectionId.studentId = :studentId")
     public List<StudentSection> findByStudentId(Integer studentId);
     public List<StudentSection> findByBlockId(Integer blockId);
-    @Query(value = "select ss from StudentSection ss where ss.studentFacultyId.studentId = :studentId and ss.blockId = :blockId")
+    @Query(value = "select ss from StudentSection ss where ss.studentSectionId.studentId = :studentId and ss.blockId = :blockId")
     public List<StudentSection> findByStudentIdAndBlockId(Integer studentId, Integer blockId);
 }
