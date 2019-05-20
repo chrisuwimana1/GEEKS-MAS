b53ebea2c352c8685e304fe411c57b909fb1c03b
chrisuwimana1 <chrisuwimana1@gmail.com>
2019-05-20 18:33:29 -0500

    stashing ui changes

--- geek-mas-client/src/main/resources/static/js/student_report.js
+++ geek-mas-client/src/main/resources/static/js/student_report.js
@@ -6,17 +6,50 @@ $(document).ready(function () {
         window.location = "http://localhost:8080/login";
     }
 
+    //Populate drop down
+    var dropdown = $('#blocksList');
 
-    var studentId = decoded.id;
-    console.log(studentId);
+    dropdown.empty();
 
+    dropdown.append('<option selected="true" disabled>Select Block</option>');
+    dropdown.prop('selectedIndex', 0);
+    $.ajax({
+        url: "http://localhost:8888/blocks",
+        headers: {
+            "token": localStorage.getItem("token")
+        },
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(data){
+            for (var i =0; i <data.length; i++) {
+                dropdown.append($('<option></option>').attr('value', data[i].id).text(data[i].name));
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+    //
+    //
+    // var studentId = decoded.id;
+    // console.log(studentId);
+    //
     // $.ajax({
-    //     url: "",
+    //     url: "http://localhost:8888//attendances/sections/students/"+studentId+"/cumul",
     //     contentType: "application/json",
     //     dataType: 'json',
+    //     headers: {
+    //         "token": localStorage.getItem("token")
+    //     },
     //     success: function(data){
     //
-    //         $("#totalSessionsPossible").text(data);
+    //        // $("#totalSessionsPossible").text(data.);
     //         $("#totalSessionsAttended").text(data);
     //         $("#attendancePercentage").text(data);
     //
@@ -27,6 +60,30 @@ $(document).ready(function () {
     //         $("#extraCredit").text(data);
     //     }
     // })
+    //
+    // $( "#blocksList" ).change(function() {
+    //
+    //     alert($("#blocksList option:selected").text());
+    //
+    //     $.ajax({
+    //         url: "http://localhost:8888//attendances/sections/students/"+studentId+"/cumul",
+    //         contentType: "application/json",
+    //         dataType: 'json',
+    //         success: function(data){
+    //
+    //             $("#sessionsInBlock").text(data);
+    //             $("#totalSessions").text(data);
+    //             $("#percentage").text(data);
+    //             $("#extraCredit").text(data);
+    //         }
+    //     })
+
+
+
+
+
+       /// alert( "Handler for .change() called." );
+    //});
 
 
 });
